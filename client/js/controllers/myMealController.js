ab7b0976080eb5dc805eee7359a0b9d474bb9c02
KierstonHill83 <khill1218@gmail.com>
2015-10-12 22:13:17 -0600

    Updated the form on personal page

--- client/js/controllers/myMealController.js
+++ client/js/controllers/myMealController.js
@@ -5,31 +5,7 @@ app.controller('myMealController', function($scope) {
   $scope.recipeInfo = false;
   $scope.mealForm = false;
   $scope.recipeInfo2 = false;
-
-  // $scope.loginTemplate = false;
-  // $scope.setLoginTemplate = function(isHidden) {
-  //   $scope.loginTemplate = isHidden;
-  // };
-
-  // $scope.homePage = false;
-  // $scope.setHomePage = function(isHidden) {
-  //   $scope.homePage = isHidden;
-  // };
-
-  // $scope.registerTemplate = false;
-  // $scope.setRegisterTemplate = function(isHidden) {
-  //   $scope.registerTemplate = isHidden;
-  // };
-
-  // $scope.personalPage = false;
-  // $scope.setPersonalPage = function(isHidden) {
-  //   $scope.personalPage = isHidden;
-  // };
-
-  // $scope.contactPage = false;
-  // $scope.setContactPage = function(isHidden) {
-  //   $scope.contactPage = isHidden;
-  // };
+  $scope.fullWeek = false;
 
   $scope.ingredients = '';
   $scope.query = '';
